234ed54c26b8bc7ae90afe44e4fb2376c2b327f5
Krasowskaya <krasovskayameow@gmail.com>
2022-10-22 21:36:27 +0300

    add new tasks

---
+++ task41/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+
+void InputArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-100,999);
+}
+
+int Counter(int[] array)
+
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите кол-во элементов массиvа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine("[" + string.Join( ", ", array) + "]");
+Console.WriteLine(Counter(array));
+
+
